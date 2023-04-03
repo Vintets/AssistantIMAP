8c6935a3b2e5f8b8a57ac7163e56fba407e2f54a
Vintets <vintets@mail.ru>
2023-04-03 16:42:38 +0300

    module imap_utf7 to package accessory

--- accessory/__init__.py
+++ accessory/__init__.py
@@ -3,6 +3,7 @@ from .clear_console import clear_console  # noqa: E402
 from .colorprint import cprint  # noqa: E402
 from .utils import check_version, create_dirs, exit_from_program  # noqa: E402
 from .log import logger  # noqa: E402
+from . import imap_utf7
 
 
 __all__ = (
@@ -13,4 +14,5 @@ __all__ = (
     'create_dirs',
     'exit_from_program',
     'logger',
+    'imap_utf7',
 )
