00ef6b5463c21cd127bd54d0958e749a424949f3
Vintets <vintets@mail.ru>
2023-04-03 16:49:08 +0300

    fix noqa: E402

--- accessory/__init__.py
+++ accessory/__init__.py
@@ -3,7 +3,7 @@ from .clear_console import clear_console  # noqa: E402
 from .colorprint import cprint  # noqa: E402
 from .utils import check_version, create_dirs, exit_from_program  # noqa: E402
 from .log import logger  # noqa: E402
-from . import imap_utf7
+from . import imap_utf7  # noqa: E402
 
 
 __all__ = (
