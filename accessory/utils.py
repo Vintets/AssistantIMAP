49f0bbbb83866a3906631fd6047949e76d5e42a7
Vintets <vintets@mail.ru>
2025-05-22 09:24:57 +0300

    Delete not used func in utils module

--- accessory/utils.py
+++ accessory/utils.py
@@ -1,12 +1,7 @@
 import os
 import sys
 import time
-import ctypes
 from enum import Enum
-
-
-MOUSEEVENTF_LEFTDOWN = 2
-MOUSEEVENTF_LEFTUP = 4
 
 
 def check_version() -> None:
@@ -20,19 +15,6 @@ def check_version() -> None:
 def create_dirs(path_graphlog) -> None:
     if not (path_graphlog.exists() and path_graphlog.is_dir()):
         path_graphlog.mkdir()
-
-
-def limit(value: int, low: int = 0, high: int = 255) -> int:
-    """Лимиты для переданного значения"""
-    return min(max(value, low), high)
-
-
-def lclick(x: int, y: int) -> None:
-    win32api = ctypes.windll.user32
-    win32api.SetCursorPos(x, y)
-    win32api.mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    time.sleep(0.3)
-    win32api.mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
 
 def exit_from_program(code: int = 0, close: bool = False) -> None:
