2baa2f872d7383f24a10dd3b84a48c4671904a19
Vintets <vintets@mail.ru>
2025-05-22 09:10:16 +0300

    Rename module 'log' to loguru_log

--- accessory/__init__.py
+++ accessory/__init__.py
@@ -2,7 +2,7 @@ from .authorship import authorship  # noqa: E402
 from .clear_console import clear_console  # noqa: E402
 from .colorprint import cprint  # noqa: E402
 from .utils import check_version, create_dirs, exit_from_program  # noqa: E402
-from .log import logger  # noqa: E402
+from .loguru_log import logger  # noqa: E402
 from . import imap_utf7  # noqa: E402
 
 
